dc2906c9421ddcae7579f0917942a1e3d112abc2
Doug Mahugh <dmahugh@microsoft.com>
2016-09-17 08:47:12 -0700

    add photo_detail()

--- flickrtags.py
+++ flickrtags.py
@@ -13,37 +13,47 @@ def get_tags_example(user_id):
     user_id = user ID; will print tags for first 20 photos in this photostream
     """
 
-    # get some photos for testing
+    # get the first 10 photos
     per_page = '10' # max=500 for production use later
     endpoint = 'https://api.flickr.com/services/rest/' + \
         '?method=flickr.people.getPhotos' + \
         '&api_key=cbfb84b23a5a7af2a0bf27cc8a87d85b&user_id=' + user_id + \
         '&per_page=' + per_page + '&format=json&nojsoncallback=1'
     response = requests.get(endpoint)
-    resp = json.loads(response.text)
+    photolist = json.loads(response.text)
 
     # iterate through the photos and display keywords/timestamp for each
-    for photo in resp['photos']['photo']:
-
-        # get info for this single photo
-        user_id = photo['owner']
-        photo_id = photo['id']
-        secret = photo['secret']
-        endpoint = 'https://api.flickr.com/services/rest/' + \
-            '?method=flickr.photos.getInfo' + \
-            '&api_key=cbfb84b23a5a7af2a0bf27cc8a87d85b&photo_id=' + photo_id + \
-            '&secret=' + secret + '&format=json&nojsoncallback=1'
-        response = requests.get(endpoint)
-        photo_info = json.loads(response.text)
+    for photo in photolist['photos']['photo']:
+        photo_info = photo_detail(photo) # photos.getInfo API call
 
         # create list of keywords (including the title)
         keywords = {tag['raw'].strip().lower() for tag in photo_info['photo']['tags']['tag']}
         keywords.add(photo['title'].strip().lower())
 
         # display a summary of this image
-        photo_url = 'http://flickr.com/photos/' + user_id + '/' + photo_id
+        photo_url = 'http://flickr.com/photos/' + user_id + '/' + photo['id']
         taken = photo_info['photo']['dates']['taken']
         print(photo_url + ' - ' + taken + ' - ' + str(keywords))
+
+#-------------------------------------------------------------------------------
+def photo_detail(photo):
+    """Get detailed information for a photo.
+
+    photo = the JSON representation of a photo as returned by the
+            people.getPhotos API call.
+
+    Returns the JSON output of the photos.getInfo API call for this photo.
+    """
+    user_id = photo['owner']
+    photo_id = photo['id']
+    secret = photo['secret']
+
+    endpoint = 'https://api.flickr.com/services/rest/' + \
+        '?method=flickr.photos.getInfo' + \
+        '&api_key=cbfb84b23a5a7af2a0bf27cc8a87d85b&photo_id=' + photo_id + \
+        '&secret=' + secret + '&format=json&nojsoncallback=1'
+    response = requests.get(endpoint)
+    return json.loads(response.text)
 
 #-------------------------------------------------------------------------------
 if __name__ == '__main__':
