ed1623cefa498af2f77bfdd05c7f34d297e4b971
Doug Mahugh <dmahugh@microsoft.com>
2016-09-17 08:14:53 -0700

    remove API signatures

--- flickrtags.py
+++ flickrtags.py
@@ -13,16 +13,12 @@ def get_tags_example(user_id):
     user_id = user ID; will print tags for first 20 photos in this photostream
     """
 
-    # try without authentication, then read this carefully:
-    # https://www.flickr.com/services/api/auth.oauth.html
-
     # get some photos for testing
     per_page = '20' # max=500 for production use later
     endpoint = 'https://api.flickr.com/services/rest/' + \
         '?method=flickr.people.getPhotos' + \
         '&api_key=cbfb84b23a5a7af2a0bf27cc8a87d85b&user_id=' + user_id + \
-        '&per_page=' + per_page + '&format=json&nojsoncallback=1' + \
-        '&api_sig=66f149b05c6db3e129a84ac0ea93001d'
+        '&per_page=' + per_page + '&format=json&nojsoncallback=1'
     response = requests.get(endpoint)
     resp = json.loads(response.text)
 
@@ -33,18 +29,17 @@ def get_tags_example(user_id):
         photo_id = photo['id']
         secret = photo['secret']
         photo_url = 'http://flickr.com/photos/' + user_id + '/' + photo_id
-        print(title + ' ' + photo_url)
+        print(photo_url)
+        print('  >> Title: ' + title)
         # now get info for this single photo
         endpoint = 'https://api.flickr.com/services/rest/' + \
             '?method=flickr.photos.getInfo' + \
             '&api_key=cbfb84b23a5a7af2a0bf27cc8a87d85b&photo_id=' + photo_id + \
-            '&secret=' + secret + '&format=json&nojsoncallback=1' + \
-            '&api_sig=630073f2a1c6ac5e08457143ebc979ac'
+            '&secret=' + secret + '&format=json&nojsoncallback=1'
         response = requests.get(endpoint)
         photo_info = json.loads(response.text)
-        print('-'*80)
-        print(str(photo_info))
-        print('-'*80)
+        taken = photo_info['photo']['dates']['taken']
+        print('  >> Taken: ' + taken)
         for tag in photo_info['photo']['tags']['tag']:
             tagname = tag['raw']
             print('  >> Tag: ' + tagname)
